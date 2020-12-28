6bc5a32b3b7f7a344ccdf8ae57d9c2b219f9436a
AlexanderLyaksa <sanchos2004@gmail.com>
2020-12-29 00:23:31 +0300

    Reafactored code.

--- main.py
+++ main.py
@@ -1,113 +1,84 @@
 import logging
 import os
-
-from logging.handlers import RotatingFileHandler
 from urllib.parse import urljoin
 
 import requests
-import urllib3
-
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from pathvalidate import sanitize_filename
 
 load_dotenv()
+logger = logging.getLogger('parser.main')
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-handler = RotatingFileHandler(  # noqa: WPS110
-    os.getenv('LOG_FILE_NAME', 'parser.log'),
-    maxBytes=102400,  # noqa: WPS432
-    backupCount=3,
-)
-formatter = logging.Formatter('%(asctime)s %(process)d %(levelname)s %(message)s')  # noqa: WPS323
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+
+def check_for_redirect(response):
+    """Проверяет наличие редиректа."""
+    if response.history:
+        raise requests.exceptions.HTTPError
 
 
 def get_response(url):
-    """Функция для получения ответа на запрос по url."""
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    """получает ответ на запрос по url."""
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0',
     }
     response = requests.get(url=url, headers=headers, verify=False)  # noqa: S501
     response.raise_for_status()
+    check_for_redirect(response)
     return response
 
 
-def response_handler(url):
-    """Функция обработки ответа на запрос."""
-    try:
-        response = get_response(url)
-    except requests.HTTPError as err:
-        logger.error(err, exc_info=True)
-    else:
-        return response
-
-
-def get_book_description(url):
-    """Функция для парсинга названия книги и автора."""
-    response = response_handler(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    selector = '.ow_px_td h1'
-    title = soup.select_one(selector)
+def get_book_description(soup):
+    """Получает название книги и автора."""
+    title = soup.select_one('.ow_px_td h1')
     return [text.strip() for text in title.text.split('::')]
 
 
-def get_book_genres(url):
-    """Функция получения жанров книги."""
-    response = response_handler(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    selector = '.ow_px_td span.d_book a'
-    raw_genres = soup.select(selector)
+def get_book_genres(soup):
+    """Получает жанр книги."""
+    raw_genres = soup.select('.ow_px_td span.d_book a')
     if not raw_genres:
         return None
-    genres = []
-    for genre in raw_genres:
-        genres.append(genre.text)
-    return genres
+    return [genre.text for genre in raw_genres]
 
 
-def get_book_comments(url):
-    """Функция для скачивания коментариев к книге."""
-    response = response_handler(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    selector = '.texts span'
-    comment_tags = soup.select(selector)
+def get_book_comments(soup):
+    """Получает коментарии к книге."""
+    comment_tags = soup.select('.texts span')
     if not comment_tags:
         return None
-    comments = []
-    for comment in comment_tags:
-        comments.append(comment.text)
-    return comments
+    return [comment.text for comment in comment_tags]
+
+
+def create_dir(path, folder):
+    """Создает директорию."""
+    directory = os.path.join(path, folder)
+    os.makedirs(directory, exist_ok=True)
+    return directory
 
 
 def download_img(url, path, folder='image/'):
-    """Функция для скачивания обложек."""
-    directory = os.path.join(path, folder)
-    os.makedirs(directory, exist_ok=True)
-    response = response_handler(url)
+    """Скачивает обложку книги."""
+    directory = create_dir(path, folder)
+    response = get_response(url)
     soup = BeautifulSoup(response.text, 'lxml')
-    selector = '.bookimage img'
-    img = soup.select_one(selector)
+    img = soup.select_one('.bookimage img')
     if not img:
         return None
     abs_img_url = urljoin('https://tululu.org/', img['src'])
     img_name = abs_img_url.split('/')[-1]
     path_to_file = os.path.join(directory, img_name)
-    raw_img = response_handler(abs_img_url)
+    raw_img = get_response(abs_img_url)
     with open(path_to_file, 'wb') as image:
         image.write(raw_img.content)
     return path_to_file
 
 
 def download_txt(url, filename, path, folder='books/'):
-    """Функция для скачивания текстовых файлов."""
-    directory = os.path.join(path, folder)
-    os.makedirs(directory, exist_ok=True)
+    """Скачивает текст книги."""
+    directory = create_dir(path, folder)
     filename = sanitize_filename(filename)
-    response = response_handler(url)
+    response = get_response(url)
     path_to_file = os.path.join(directory, f'{filename}.txt')
     if response.headers['Content-Type'] == 'text/plain; charset="utf-8"':
         with open(path_to_file, 'w', encoding='utf8') as book:
--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,20 +1,25 @@
 import argparse
 import json
+import logging.config
 import os
+import yaml
 from urllib.parse import urljoin
 
+import requests
+import urllib3
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from tqdm import tqdm
 
 from main import download_img, download_txt, get_book_comments
-from main import get_book_description, get_book_genres, logger, response_handler
+from main import get_book_description, get_book_genres, get_response
 
 load_dotenv()
+logger = logging.getLogger('parser')
 
 
 def create_parser():
-    """Парсер аргументов запуска скрипта."""
+    """Создание аргументов запуска скрипта."""
     parser = argparse.ArgumentParser()
     parser.add_argument('--start_page', help='Начальная страница', required=True, type=int)
     parser.add_argument(
@@ -35,40 +40,72 @@ def create_parser():
 
 
 def get_rel_book_urls(url):
-    """Получение списка относительных адресов книг."""
-    response = response_handler(url)
+    """Получает список относительных адресов книг."""
+    response = get_response(url)
     soup = BeautifulSoup(response.text, 'lxml')
-    selector = '.tabs .d_book .bookimage a'
-    tags = soup.select(selector)
+    tags = soup.select('.tabs .d_book .bookimage a')
     if not tags:
         return None
     return [tag['href'] for tag in tags]
 
 
+def configure_logging():
+    """Конфигурирует работу логгера."""
+    try:
+        with open(os.getenv('LOG_CONFIG'), 'r') as log_config:
+            config = yaml.safe_load(log_config.read())
+            logging.config.dictConfig(config)
+    except FileNotFoundError as error:
+        logging.basicConfig(level=logging.ERROR)
+        logger.error(error, exc_info=False)
+
+
 if __name__ == '__main__':
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    configure_logging()
     parser = create_parser()
     namespace = parser.parse_args()
     logger.info(f'Start parsing with params: {namespace}')
     path, filename = os.path.split(namespace.json_path)
     os.makedirs(path, exist_ok=True)
-    # Если указан путь только к директории json файла то имя файла возьмем из переменной окружения.
     if not filename:
         filename = os.getenv('FILE_NAME', 'description.json')
     books = []
     for page in range(namespace.start_page, namespace.end_page):
         url = f'https://tululu.org/l55/{page}'
-        book_rel_urls = get_rel_book_urls(url)
-        if not book_rel_urls:
-            break
+        try:
+            book_rel_urls = get_rel_book_urls(url)
+        except requests.exceptions.HTTPError as error:
+            logger.error(error, exc_info=True)
+            continue
         for rel_url in tqdm(book_rel_urls):
             abs_url = urljoin(url, rel_url)
-            title, author = get_book_description(abs_url)
-            img_src = None if namespace.skip_imgs else download_img(abs_url, namespace.dest_folder)
-            book_id = rel_url.split('/b')[-1]
-            book_url = f'https://tululu.org/txt.php?id={book_id}'
-            book_path = None if namespace.skip_txt else download_txt(book_url, title, namespace.dest_folder)
-            comments = get_book_comments(abs_url)
-            genres = get_book_genres(abs_url)
+            try:
+                response = get_response(abs_url)
+            except requests.exceptions.HTTPError as error:
+                logger.error(error, exc_info=True)
+                continue
+            soup = BeautifulSoup(response.text, 'lxml')
+            title, author = get_book_description(soup)
+            comments = get_book_comments(soup)
+            genres = get_book_genres(soup)
+            if namespace.skip_imgs:
+                img_src = None
+            else:
+                try:
+                    img_src = download_img(abs_url, namespace.dest_folder)
+                except requests.exceptions.HTTPError as error:
+                    logger.error(error, exc_info=True)
+                    continue
+            book_url = f'https://tululu.org/txt.php?id={rel_url.split("/b")[-1]}'
+            if namespace.skip_txt:
+                book_path = None
+            else:
+                try:
+                    book_path = download_txt(book_url, title, namespace.dest_folder)
+                except requests.exceptions.HTTPError as error:
+                    logger.error(error, exc_info=True)
+                    continue
             specification = {
                 'title': title,
                 'author': author,
